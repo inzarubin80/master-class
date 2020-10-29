2889c50836ea7fa6360de5ab3ecda5dfb67dc6c0
Ivan <inzarubin80@gmail.com>
2020-10-29 23:02:31 +0300

    init

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAodUl-wg7JK7tNHyj0_Gg1tB4xEQjTsk8",
+  authDomain: "master-class-com.firebaseapp.com",
+  databaseURL: "https://master-class-com.firebaseio.com",
+  projectId: "master-class-com",
+  storageBucket: "master-class-com.appspot.com",
+  messagingSenderId: "854633369027",
+  appId: "1:854633369027:web:8e6b312a5005f295a3c176",
+  measurementId: "G-YF2D83PT2N"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
 
 ReactDOM.render(
   <React.StrictMode>
