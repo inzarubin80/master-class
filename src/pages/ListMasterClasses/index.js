b559a2304f1b9e18ff0aa1ab4bf233be16ed01d9
Ivan <inzarubin80@gmail.com>
2020-11-10 10:05:39 +0300

    +

--- src/pages/ListMasterClasses/index.js
+++ src/pages/ListMasterClasses/index.js
@@ -1,15 +1,58 @@
-import React from 'react'
-
+import React, { useState, useEffect} from 'react'
 import MasterСlass from '../MasterСlass';
+import { db} from '../../firebase';
+
 
 const ListMasterClasses = (props) => {
+        
+
+    const refMasterClass = db.ref('masterClass');
+
+
+    const [firstKnownKey, setFirstKnownKey] = useState(0);
+    const [data, setData] = useState([]);
+
+
+    useEffect(() => {
+      
+        //let newData = [];
+        refMasterClass.orderByKey().limitToLast(5).on('child_added', function(childSnapshot, prevChildKey) {
+            if (!firstKnownKey) {
+                setFirstKnownKey(childSnapshot.key);
+            }
+
+            //let value = childSnapshot.val();
+           // value.key = childSnapshot.key;
+
+           // console.log({...value, id:  childSnapshot.key});
+     
+            setData((prevState)=>{return [...prevState, {...childSnapshot.val(), id:  childSnapshot.key}]});
+
+        });
+
+
     
+    }, []);
+
+    
+
+
+
+
     return (<div >
         <div className="ListMasterClasses">
             <button className="btn btn-success" onClick={() => {props.history.push(`/class/-1`)}}>Добавить</button>
         </div>
-        <MasterСlass />
-
+    
+    {data.map((item)=><MasterСlass  
+    NameMasterClass = {item.NameMasterClass} 
+    ImagesMasterClass = {"https://i1.photo.2gis.com/images/branch/0/30258560050669905_3b47_300x300.jpg"} 
+    id = {item.id}
+    DescriptionMasterClass = {item.DescriptionMasterClass}
+    
+    />)
+    
+    }
     </div>);
 
 }
--- src/pages/MasterСlass/index.js
+++ src/pages/MasterСlass/index.js
@@ -8,23 +8,40 @@ const divStyle = {
 
 };
 
-const MasterClasses = () => {
+//"https://i1.photo.2gis.com/images/branch/0/30258560050669905_3b47_300x300.jpg"
+
+const MasterClasses = (props) => {
+
+   
+    console.log('входящий key ' + props.id);
+
+    console.log(props);
+
     return (
-        <div className="MasterClasses">
+       
+        // <div className="MasterClasses" key={props.key}>
+
+<li  className="MasterClasses"  key={props.id}>
+
 
             <div className="card" style={divStyle}>
 
                 <div className="card-header">
-                    Cухое валяние
+                    {props.NameMasterClass}
                 </div>
 
-                <img className="card-img-top" src="https://i1.photo.2gis.com/images/branch/0/30258560050669905_3b47_300x300.jpg" alt="Card image cap" />
+
+
+                <img className="card-img-top" src= {props.ImagesMasterClass} alt="Card image cap" />
 
                 <div className="card-body">
 
-                    <p className="card-text">Мы знаем, что среди вас много любителей Хэллоуина! Уж больно захватывающая атрибутика у этого праздника! Брр! Куда же в Хэллоуин без летучих мышей?!
+                    {/*<p className="card-text">Мы знаем, что среди вас много любителей Хэллоуина! Уж больно захватывающая атрибутика у этого праздника! Брр! Куда же в Хэллоуин без летучих мышей?!
                     По этому поводу мы подготовили для вас мастер-класс по летучей мышке!
-                    В субботу,31 октября, в 14-00.🙂</p>
+                    В субботу,31 октября, в 14-00.🙂</p>*/}
+
+                <p className="card-text">{props.DescriptionMasterClass}</p>
+
                 </div>
 
                 <button type="submit" className="btn btn-primary btn-block" onClick={() => { }}>Зарезервировать</button>
@@ -33,7 +50,7 @@ const MasterClasses = () => {
 
                 </div>
 
-            </div>
+            </li>
     );
 }
 
